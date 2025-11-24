b227a7784b1a5e35e0b2dbb4e3de3a7ca9207747
daniela_bravo <danielabravorueda@gmail.com>
2025-11-24 14:00:53 -0400

    modificacion del archivo config

--- utils/config.py
+++ utils/config.py
@@ -3,12 +3,14 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / ".env")
+env_file = BASE_DIR / ".env"
+if env_file.exists():
+    load_dotenv(env_file)
 
 BASE_URI = os.getenv("BASE_URI")
 USER_EMAIL = os.getenv("USER_EMAIL")
 USER_PASSWORD = os.getenv("USER_PASSWORD")
 
-
 if not USER_EMAIL or not USER_PASSWORD:
-    raise RuntimeError(" Error: No se cargaron las variables desde .env")
+    print("Variables no están cargadas (puede ser normal en CI)")
+
